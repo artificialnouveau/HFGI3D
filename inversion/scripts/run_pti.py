dc1ac7bbaf3678b624dbd8e98fd3e62c813fc5df
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-11-15 14:28:07 +0100

    Update run_pti.py

--- inversion/scripts/run_pti.py
+++ inversion/scripts/run_pti.py
@@ -20,6 +20,15 @@ def run_PTI(path_config,run_name='',multi_views=False,use_wandb=False, use_multi
     os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
+    # Check for CUDA availability
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("CUDA is available. Using device:", device)
+    else:
+        device = torch.device("cpu")
+        print("CUDA is not available. Using CPU.")
+
+
     if run_name == '':
         global_config.run_name = ''.join(choice(ascii_uppercase) for i in range(12))
     else:
@@ -32,22 +41,23 @@ def run_PTI(path_config,run_name='',multi_views=False,use_wandb=False, use_multi
 
     embedding_dir_path = f'{paths_config.embedding_base_dir}/{paths_config.input_data_id}/{paths_config.pti_results_keyword}'
     os.makedirs(embedding_dir_path, exist_ok=True)
-
-    dataset = ImagesDataset(paths_config.input_data_path, paths_config.name, transforms.Compose([
+    
+    # Load dataset and initialize DataLoader
+    dataset = ImagesDataset(path_config.input_data_path, path_config.name, transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]))
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, pin_memory=True)
 
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
-
+    # Initialize the appropriate coach
     if use_multi_id_training:
-        coach = MultiIDCoach(dataloader, paths_config,multi_views,use_wandb)
+        coach = MultiIDCoach(dataloader, path_config, multi_views, use_wandb, device)
     else:
-        coach = SingleIDCoach(dataloader,paths_config,multi_views, use_wandb)
+        coach = SingleIDCoach(dataloader, path_config, multi_views, use_wandb, device)
 
+    # Train the model
     coach.train()
 
     return global_config.run_name
-
 
 if __name__ == '__main__':
 
